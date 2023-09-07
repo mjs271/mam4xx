9529bc52273bfc351d2fa403f38493e96d405868
Jaelyn Litzinger <jaelyn.litzinger@pnnl.gov>
2023-09-07 16:38:17 -0700

    compiles and test runs

--- src/validation/mo_chm_diags/het_diags.cpp
+++ src/validation/mo_chm_diags/het_diags.cpp
@@ -14,24 +14,56 @@ using namespace mam4;
 using namespace haero;
 using namespace mo_chm_diags;
 
-constexpr int gas_pcnst = gas_chemistry::gas_pcnst;
+//constexpr const int gas_pcnst = gas_chemistry::gas_pcnst;
 
 void het_diags(Ensemble *ensemble) {
   ensemble->process([=](const Input &input, Output &output) {
   
-    const auto ncol = input.get_array("ncol")[0];
     const auto het_rates_in = input.get_array("het_rates");
     const auto mmr_in = input.get_array("mmr");
     const auto pdel_in = input.get_array("pdel");
     const auto wght = input.get_array("wght")[0];
 
+    Real het_rates[pver][gas_pcnst];
+    Real mmr[pver][gas_pcnst];
+    Real pdel[pver];
     Real wrk_wd[gas_pcnst];
-    Real sox_wk[gas_pcnst];
-    Real adv_mass[gas_pcnst];
+    Real sox_wk;
+    Real sox_species[3] = {4, -1, 3};
+    Real adv_mass[gas_pcnst] = {47.998200,      34.013600,      98.078400,      64.064800,      62.132400, 
+                               12.011000,     115.107340,      12.011000,      12.011000,      12.011000, 
+                              135.064039,      58.442468,  250092.672000,       1.007400,     115.107340, 
+                               12.011000,      58.442468,  250092.672000,       1.007400,     135.064039, 
+                               58.442468,     115.107340,      12.011000,      12.011000,      12.011000, 
+                           250092.672000,       1.007400,      12.011000,      12.011000,  250092.672000, 
+                                1.007400};
 
-    mo_chm_diags::het_diags(het_rates, mmr, pdel, wght, wrk_wd, sox_wk, adv_mass); 
+    int counter = 0;
+    for(int mm = 0; mm < gas_pcnst; mm++) {
+      for(int k = 0; k < pver; k++) {
+        het_rates[k][mm] = het_rates_in[counter];
+        mmr[k][mm] = mmr_in[counter];
+        counter++;
+      }
+      wrk_wd[mm] = 0;
+    }
+    sox_wk = 0;
 
-    output.set("wrk_wd_mm", wrk_wd);
+    counter = 0;
+    for(int k = 0; k < pver; k++) {
+      pdel[k] = pdel_in[counter];
+      counter++;
+    }
+
+    mo_chm_diags::het_diags(het_rates, mmr, pdel, wght, wrk_wd, sox_wk, adv_mass, sox_species); 
+
+    std::vector<Real> wrk_wd_mm(gas_pcnst);
+    counter = 0;
+    for(int mm = 0; mm < gas_pcnst; mm++) {
+      wrk_wd_mm[counter] = wrk_wd[mm];
+    }
+
+    output.set("wrk_wd_mm", wrk_wd_mm);
     output.set("sox_wk", sox_wk);
 
   });
